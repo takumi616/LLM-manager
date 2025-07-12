8f65f5beee1a215c28f08f6ac6f8dbbc01133c89
takumi616 <ohohyeah321@icloud.com>
2025-07-13 00:50:04 +0900

    Modify returned type and remove print function

--- app/crud/llm.py
+++ app/crud/llm.py
@@ -2,9 +2,8 @@ from app.core.database import SessionDep
 from app.models.llms import LargeLanguageModel, LargeLanguageModelCreate, LargeLanguageModelPublic
 
 
-async def register_llm(session: SessionDep, llm: LargeLanguageModelCreate) -> LargeLanguageModelPublic:
+async def register_llm(session: SessionDep, llm: LargeLanguageModelCreate) -> LargeLanguageModel:
     db_llm = LargeLanguageModel.model_validate(llm)
-    print(db_llm)
     session.add(db_llm)
     await session.commit()
     await session.refresh(db_llm)
