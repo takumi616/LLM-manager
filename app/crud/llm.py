5d131ff821e1b47f348add37cdaee5962939482d
takumi616 <ohohyeah321@icloud.com>
2025-07-13 09:38:00 +0900

    Modify function names

--- app/crud/llm.py
+++ app/crud/llm.py
@@ -3,7 +3,7 @@ from app.core.database import SessionDep
 from app.models.llms import LargeLanguageModel, LargeLanguageModelCreate
 
 
-async def register_llm(session: SessionDep, llm: LargeLanguageModelCreate) -> LargeLanguageModel:
+async def insert_llm(session: SessionDep, llm: LargeLanguageModelCreate) -> LargeLanguageModel:
     db_llm = LargeLanguageModel.model_validate(llm)
     session.add(db_llm)
     await session.commit()
@@ -11,7 +11,7 @@ async def register_llm(session: SessionDep, llm: LargeLanguageModelCreate) -> La
     return db_llm
 
 
-async def get_llm_by_id(session: SessionDep, id: str) -> LargeLanguageModel:
+async def select_llm_by_id(session: SessionDep, id: str) -> LargeLanguageModel:
     stmt = select(LargeLanguageModel).where(LargeLanguageModel.id == id)
     result = await session.execute(stmt)
     db_llm = result.scalars().first()
